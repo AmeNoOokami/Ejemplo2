80ec20bd5f59ef11dffa4df2b113118e6fc95139
Stiven <31933572+AmeNoOokami@users.noreply.github.com>
2017-09-22 21:30:25 -0500

    colocando un comentario
    terminando de colocar una oracion que le faltaba en el comentario de la linea numero 27

--- example/src/com/company/Main.java
+++ example/src/com/company/Main.java
@@ -24,7 +24,7 @@ public class Main {
 
         // Boleano
         boolean vivo = true; // true es en minuscula
-        vivo = false  // false va en
+        vivo = false  // false va en minuscula
 
 
     }
