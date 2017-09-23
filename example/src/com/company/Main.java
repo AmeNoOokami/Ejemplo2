7b07d9bd7b1e3b61882304eef8ba017784ef6b4e
Stiven <31933572+AmeNoOokami@users.noreply.github.com>
2017-09-22 21:27:24 -0500

    explicacion de variables
    se explican las variables que se usan.

---
+++ example/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a Java !");
+
+        //todo lo que inicia con mayuscula es una clase
+
+        //Enteros
+        byte a; //entero de 8 byte
+        short b; //entero de 16 byte
+        int c = 27; //entero de 32 byte
+        long d; //entero de 64 byte
+
+
+        // Decimales
+        float e; //decimales flotantes
+        double f = 0.0;
+
+        //Texto
+        char g = 'x'; //para un solo caracter, es obligatorio ponerle el apostrofi
+        String i = "x"; // para los caracteres que tienen mas de un caracter, es obligatorio ponerle las comillas
+
+        // Boleano
+        boolean vivo = true; // true es en minuscula
+        vivo = false  // false va en
+
+
+    }
+}
